f2e85a9caf75cdecb7728f470b7920a6f0ec9859
TAN <lrr1225@naver.com>
2025-04-03 17:16:49 +0900

    Add files via upload

---
+++ js/global_navigation.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".menu").click(function(){
+        $(".nav_menu").toggleClass("gnb_slider");
+        // $().();
+        $(this).toggleClass("open");
+    });
+});
